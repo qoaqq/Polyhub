2a8be3277a065bdfaf7d17a09f65340c9080e2a7
qoaqq <129212733+qoaqq@users.noreply.github.com>
2024-05-15 14:06:35 +0700

    commit push code

---
+++ database/migrations/2024_05_15_070120_create_showing_release_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('showing_release', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('movie_id')->constrained();
+            $table->foreignId('ticket_id')->constrained();
+            $table->foreignId('seat_id')->constrained();
+            $table->foreignId('room_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('showing_release');
+    }
+};
