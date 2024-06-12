a4b706c56208517a3b0c82d9bb5a6b43d08b1eaf
huy nguyen <huytdhd@gmail.com>
2024-06-12 21:12:33 +0700

    created view of cinamatype

--- Modules/CinemaType/Resources/views/create.blade.php
+++ Modules/CinemaType/Resources/views/create.blade.php
@@ -4,13 +4,13 @@
     {{-- nav start --}}
     <div class="card shadow-none position-relative overflow-hidden mb-4">
         <div class="card-body d-flex align-items-center justify-content-between p-4">
-            <h4 class="fw-semibold mb-0">Chart Apex Radial</h4>
+            <h4 class="fw-semibold mb-0">Add new cinema type</h4>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mb-0">
                     <li class="breadcrumb-item">
                         <a class="text-muted text-decoration-none" href="../dark/index.html">Home</a>
                     </li>
-                    <li class="breadcrumb-item" aria-current="page">Chart Apex Radial</li>
+                    <li class="breadcrumb-item" aria-current="page">cinema type</li>
                 </ol>
             </nav>
         </div>
