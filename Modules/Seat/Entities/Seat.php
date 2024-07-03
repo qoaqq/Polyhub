fbfc861c2f4d8779a7bffc5cf2f9e1c999aee4b6
huy nguyen <huytdhd@gmail.com>
2024-07-03 21:44:21 +0700

    city api

--- Modules/Seat/Entities/Seat.php
+++ Modules/Seat/Entities/Seat.php
@@ -4,11 +4,13 @@ namespace Modules\Seat\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Modules\Room\Entities\Room;
 
 class Seat extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['row', 'column','status', 'type', 'room_id'];
 
--- Modules/Seat/Routes/api.php
+++ Modules/Seat/Routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Modules\Seat\Http\Controllers\Api\SeatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/seat', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('cities', SeatController::class);
