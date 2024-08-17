bf9156b12603685737f68ba334fc9a275e1599bd
qoaqq <129212733+qoaqq@users.noreply.github.com>
2024-08-17 11:39:35 +0700

    update print invoie by quangduoc

--- app/Http/Controllers/Backend/BackendControllerBase.php
+++ app/Http/Controllers/Backend/BackendControllerBase.php
@@ -2,13 +2,26 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class BackendControllerBase extends Controller
 {
     //
     public function index() {
         return view('Backend.index');
+    }
+
+    public function getProductsData()
+    {
+        $data = DB::table('orders')
+        ->select(DB::raw('DATE(order_date) as date'), DB::raw('COUNT(product_id) as sold_products'))
+        ->where('order_date', '>=', now()->subDays(7))
+            ->groupBy(DB::raw('DATE(order_date)'))
+            ->orderBy('date')
+            ->get();
+
+        return response()->json($data);
     }
 }
